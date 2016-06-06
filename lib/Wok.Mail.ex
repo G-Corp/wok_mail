c2157e41a61fdf48b2e82aaf4f153f9d14927408
glejeune <gregoire.lejeune@free.fr>
2016-06-06 12:10:58 +0200

    Update Elixir libs

--- lib/Wok.Mail.ex
+++ lib/Wok.Mail.ex
@@ -1,15 +1,15 @@
-# File: lib/Wok.Mail.ex
-# This file was generated from src/wok_mail.erl
-# Using mix.mk (https://github.com/botsunit/mix.mk)
+# File: Wok.Mail.ex
+# This file was generated from wok_mail.beam
+# Using rebar3_elixir (https://github.com/botsunit/rebar3_elixir)
 # MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
 defmodule Wok.Mail do
-	def unquote(:"deliver")(arg1, arg2, arg3) do
-		:erlang.apply(:"wok_mail", :"deliver", [arg1, arg2, arg3])
-	end
-	def unquote(:"deliver")(arg1, arg2, arg3, arg4) do
-		:erlang.apply(:"wok_mail", :"deliver", [arg1, arg2, arg3, arg4])
-	end
-	def unquote(:"send")(arg1, arg2, arg3, arg4) do
-		:erlang.apply(:"wok_mail", :"send", [arg1, arg2, arg3, arg4])
-	end
+  def unquote(:"deliver")(arg1, arg2, arg3) do
+    :erlang.apply(:"wok_mail", :"deliver", [arg1, arg2, arg3])
+  end
+  def unquote(:"deliver")(arg1, arg2, arg3, arg4) do
+    :erlang.apply(:"wok_mail", :"deliver", [arg1, arg2, arg3, arg4])
+  end
+  def unquote(:"send")(arg1, arg2, arg3, arg4) do
+    :erlang.apply(:"wok_mail", :"send", [arg1, arg2, arg3, arg4])
+  end
 end
